0c775d0b50197a960f44c53112c981fc2a37f159
OEmiliatanO <emiliaistruelove@gmail.com>
2023-02-27 17:20:42 +0800

    remove redundant file

--- scores.sh
+++
@@ -1,3 +0,0 @@
-python score_networks.py --trainval --augtype none --repeat 1 --score hook_logdet --sigma 0.05 --nasspace nasbench201 --batch_size 128 --GPU 1 --dataset cifar10
-#python score_networks.py --trainval --augtype none --repeat 1 --score hook_logdet --sigma 0.05 --nasspace nasbench201 --batch_size 128 --GPU 1 --dataset cifar100 --data_loc ../cifar100/
-#python score_networks.py --trainval --augtype none --repeat 1 --score hook_logdet --sigma 0.05 --nasspace nasbench201 --batch_size 128 --GPU 1 --dataset ImageNet16-120 --data_loc ../imagenet16/Imagenet16/
