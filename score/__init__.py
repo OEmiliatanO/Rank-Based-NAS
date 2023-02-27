beec0cc31d5b09dafff7a08171cb1c20dc847aae
OEmiliatanO <emiliaistruelove@gmail.com>
2023-02-27 16:31:58 +0800

    update score/

---
+++ score/__init__.py
@@ -0,0 +1,4 @@
+from utils import add_dropout, init_network
+from .ntk_score import ntk_score
+from .ninaswot_score import ninaswot_score, score_gu, score_nas
+from .entropy_score import entropy_score
