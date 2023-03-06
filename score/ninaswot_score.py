de69a6749b6e2aff48db70db90d4949a6c9f80c6
OEmiliatanO <emiliaistruelove@gmail.com>
2023-03-07 01:52:01 +0800

    remove redundant code

--- score/ninaswot_score.py
+++ score/ninaswot_score.py
@@ -6,18 +6,6 @@ def hooklogdet(K, labels=None):
     s, ld = np.linalg.slogdet(K)
     return ld
 
-def random_score(jacob, label=None):
-    return np.random.normal()
-
-
-_scores = {
-        'hook_logdet': hooklogdet,
-        'random': random_score
-        }
-
-def get_score_func(score_name):
-    return _scores[score_name]
-
 def get_batch_jacobian(net, x, target, device, args=None):
     net.zero_grad()
     x.requires_grad_(True)
@@ -27,36 +15,30 @@ def get_batch_jacobian(net, x, target, device, args=None):
     return jacob, target.detach(), y.detach(), out.detach()
 
 def naswot_score(network, train_loader, device, args):
-    #print(f"before naswot cuda memory allocated = {torch.cuda.memory_allocated(0)/1024/1024/1024}")
-    if args.dropout:
-        add_dropout(network, args.sigma)
-    if args.init != '':
-        init_network(network, args.init)
-    if 'hook_' in args.score:
-        network.K = np.zeros((args.batch_size, args.batch_size))
-        def counting_forward_hook(module, inp, out):
-            try:
-                if not module.visited_backwards:
-                    return
-                if isinstance(inp, tuple):
-                    inp = inp[0]
-                inp = inp.view(inp.size(0), -1)
-                x = (inp > 0).float()
-                K = x @ x.t()
-                K2 = (1.-x) @ (1.-x.t())
-                network.K = network.K + K.cpu().numpy() + K2.cpu().numpy()
-            except:
-                pass
-                
-        def counting_backward_hook(module, inp, out):    
-            module.visited_backwards = True    
+    network.K = np.zeros((args.batch_size, args.batch_size))
+    def counting_forward_hook(module, inp, out):
+        try:
+            if not module.visited_backwards:
+                return
+            if isinstance(inp, tuple):
+                inp = inp[0]
+            inp = inp.view(inp.size(0), -1)
+            x = (inp > 0).float()
+            K = x @ x.t()
+            K2 = (1.-x) @ (1.-x.t())
+            network.K = network.K + K.cpu().numpy() + K2.cpu().numpy()
+        except:
+            pass
+            
+    def counting_backward_hook(module, inp, out):    
+        module.visited_backwards = True    
 
-        forward_handler  = []
-        backward_handler = []
-        for name, module in network.named_modules():    
-            if 'ReLU' in str(type(module)):
-                forward_handler.append(module.register_forward_hook(counting_forward_hook))
-                backward_handler.append(module.register_backward_hook(counting_backward_hook))
+    forward_handler  = []
+    backward_handler = []
+    for name, module in network.named_modules():    
+        if 'ReLU' in str(type(module)):
+            forward_handler.append(module.register_forward_hook(counting_forward_hook))
+            backward_handler.append(module.register_backward_hook(counting_backward_hook))
 
     network = network.to(device)    
     s = []
@@ -68,13 +50,9 @@ def naswot_score(network, train_loader, device, args):
     x, target = x.to(device), target.to(device)
     jacobs, labels, y, out = get_batch_jacobian(network, x, target, device, args)
 
-    if 'hook_' in args.score:
-        network(x2.to(device))
-        s.append(get_score_func(args.score)(network.K, target))
-    else:
-        s.append(get_score_func(args.score)(jacobs, labels))
+    network(x2.to(device))
+    s.append(hooklogdet(network.K, target))
     
-    #print(f"after naswot cuda memory allocated = {torch.cuda.memory_allocated(0)/1024/1024/1024}")
     for i in range(len(forward_handler)):
         forward_handler[i].remove()
         backward_handler[i].remove()
@@ -85,7 +63,6 @@ def naswot_score(network, train_loader, device, args):
 
 @torch.no_grad()
 def ni_score(network, train_loader, device, args):
-    #print(f"before ni cuda memory allocated = {torch.cuda.memory_allocated(0)/1024/1024/1024}")
     network = network.to(device)
     data_iter = iter(train_loader)
     x, target = next(data_iter)
