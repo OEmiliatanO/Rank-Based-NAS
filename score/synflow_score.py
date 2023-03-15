479967983400a806ae6d84081caabff4659bc229
OEmiliatanO <emiliaistruelove@gmail.com>
2023-03-15 22:02:12 +0800

    update synflow score function

---
+++ score/synflow_score.py
@@ -0,0 +1,48 @@
+import pytorch
+import pytorch.nn as nn
+import numpy as np
+
+def synflow_score(network, train_loader, device):
+    @torch.no_grad()
+    def linearize(network):
+        signs = {}
+        for name, param in network.state_dict().items():
+            signs[name] = torch.sign(param)
+            params.abs_()
+        return signs
+
+    @torch.no_grad()
+    def nonlinearize(network, signs):
+        for name, param in network.state_dict().items():
+            param.mul_(signs[name])
+    signs = linearize(network)
+
+    data, _ = next(iter(train_loader))
+    input_dim = list(data[0,:].shape)
+    input = torch.ones([1]+input_dim).to(device)
+    output = network(input)
+    torch.sum(output).backward()
+
+    grads_abs = []
+    
+    def synflow(layer):
+        if layer.weight.grad is not None:
+            return torch.abs(layer.weight.grad * layer.weight)
+        else:
+            return torch.zeros_like(layer.weight)
+
+    for layer in network.modules():
+        if isinstance(layer, nn.Conv2d) or isinstance(layer, nn.Linear):
+            grads_abs.append(synflow(layer))
+
+    """
+    for p in parameters():
+        scores[id(p)] = torch.clone(np.log(p.grad) * p).detach().abs_().cpu().numpy()
+        p.grad.data.zero_()
+    """
+    score = 0
+    for i in range(len(grads_abs)):
+        score += torch.sum(grads_abs[i])
+
+    nonlinearize(network, signs)
+    return score.cpu().numpy()
