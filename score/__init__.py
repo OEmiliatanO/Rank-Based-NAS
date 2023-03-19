5006faef83829a0308ba71799fa73225f5d2b91c
OEmiliatanO <emiliaistruelove@gmail.com>
2023-03-20 03:37:03 +0800

    add logsynflow

--- score/__init__.py
+++ score/__init__.py
@@ -5,6 +5,7 @@ from .ninaswot_score import ninaswot_score, ni_score, naswot_score
 #from .entropy_score import entropy_score, init_net_gaussian
 #from .gradsign_score import gradsign_score
 from .synflow_score import synflow_score
+from .logsynflow_score import logsynflow_score
 
 import numpy as np
 from tqdm import tqdm
@@ -21,6 +22,7 @@ def get_mean_std(searchspace, sample_n, train_loader, device, args):
     #scores_gradsign = []
     scores_ntk      = []
     scores_synflow  = []
+    scores_logsynflow  = []
     arches = np.random.randint(0, len(searchspace), sample_n)
     nruns = tqdm(total = sample_n)
     times = []
@@ -35,6 +37,8 @@ def get_mean_std(searchspace, sample_n, train_loader, device, args):
         #scores_ntk.append(ntk_score(network, train_loader, device))
         scores_ntk.append(0)
         scores_synflow.append(synflow_score(network, train_loader, device))
+        scores_logsynflow.append(logsynflow_score(network, train_loader, device))
+        
         #scores_gradsign.append(gradsign_score(network, train_loader, device))
         #network = init_net_gaussian(network, device)
         #scores_entropy.append(entropy_score(network, train_loader, device, args))
@@ -47,15 +51,17 @@ def get_mean_std(searchspace, sample_n, train_loader, device, args):
     scores_ni       = np.array(scores_ni)
     scores_ntk      = np.array(scores_ntk)
     scores_synflow  = np.array(scores_synflow)
+    scores_logsynflow  = np.array(scores_logsynflow)
+    
     #scores_entropy = np.array(scores_entropy)
     #scores_gradsign = np.array(scores_gradsign)
     
     calstd  = lambda x: np.ma.masked_invalid(x).std()
     calmean = lambda x: np.ma.masked_invalid(x).mean()
-    stds  = {"naswot": calstd(scores_naswot), "ni": calstd(scores_ni), "ntk": calstd(scores_ntk), "ninaswot": np.sqrt(5), "synflow": calstd(scores_synflow)}
-    means = {"naswot": calmean(scores_naswot), "ni": calmean(scores_ni), "ntk": calmean(scores_ntk), "ninaswot": 0, "synflow": calmean(scores_synflow)}
+    stds  = {"naswot": calstd(scores_naswot), "ni": calstd(scores_ni), "ntk": calstd(scores_ntk), "ninaswot": np.sqrt(5), "synflow": calstd(scores_synflow), "logsynflow": calstd(scores_logsynflow)}
+    means = {"naswot": calmean(scores_naswot), "ni": calmean(scores_ni), "ntk": calmean(scores_ntk), "ninaswot": 0, "synflow": calmean(scores_synflow), "logsynflow": calmean(scores_logsynflow)}
     
-    del scores_naswot, scores_ni, scores_ntk, scores_synflow, times
+    del scores_naswot, scores_ni, scores_ntk, scores_synflow, scores_logsynflow, times
 
     return means, stds
 
