34055306e4310e4ce65de972306e0f6c766dea4a
OEmiliatanO <emiliaistruelove@gmail.com>
2023-04-16 01:04:42 +0800

    using new ni score function

--- search.py
+++ search.py
@@ -5,17 +5,18 @@ import random
 import numpy as np
 import torch
 import os
-from scores import get_score_func
 from sklearn.metrics.pairwise import cosine_similarity
 from tqdm import trange
 from statistics import mean
+from numpy import std
 import time
 from utils import add_dropout
+from score import *
 
 
 parser = argparse.ArgumentParser(description='NAS Without Training')
 parser.add_argument('--data_loc', default='../cifardata/', type=str, help='dataset folder')
-parser.add_argument('--api_loc', default='../NAS-Bench-201-v1_0-e61699.pth',
+parser.add_argument('--api_loc', default='./NAS-Bench-201.pth',
                     type=str, help='path to API')
 parser.add_argument('--save_loc', default='results/ICML', type=str, help='folder to save results')
 parser.add_argument('--save_string', default='naswot', type=str, help='prefix of results file')
@@ -30,7 +31,11 @@ parser.add_argument('--sigma', default=0.05, type=float, help='noise level if au
 parser.add_argument('--GPU', default='0', type=str)
 parser.add_argument('--seed', default=1, type=int)
 parser.add_argument('--init', default='', type=str)
-parser.add_argument('--trainval', action='store_true')
+
+parser.add_argument('--valid', action='store_true')
+parser.add_argument('--test', action='store_true')
+parser.add_argument('--train', action='store_true')
+
 parser.add_argument('--activations', action='store_true')
 parser.add_argument('--cosine', action='store_true')
 parser.add_argument('--dataset', default='cifar10', type=str)
@@ -43,6 +48,11 @@ parser.add_argument('--num_labels', default=1, type=int, help='#classes (nasbenc
 
 args = parser.parse_args()
 os.environ['CUDA_VISIBLE_DEVICES'] = args.GPU
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+print(f"Use GPU {args.GPU}...")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(f"The current device used is {device}")
 
 # Reproducibility
 torch.backends.cudnn.deterministic = True
@@ -51,134 +61,116 @@ random.seed(args.seed)
 np.random.seed(args.seed)
 torch.manual_seed(args.seed)
 
+def remap_dataset_names(dataset, valid, test, train):
+    cifar10 = 'cifar10'
+    if dataset == cifar10 and valid:
+        return cifar10 + '-valid', 'x-valid'
+    if dataset == cifar10 and test:
+        return cifar10, 'ori-test'
+    if dataset == cifar10 and train:
+        return cifar10, 'train'
 
-def get_batch_jacobian(net, x, target, device, args=None):
-    net.zero_grad()
-    x.requires_grad_(True)
-    y, ints = net(x)
-    y.backward(torch.ones_like(y))
-    jacob = x.grad.detach()
-    return jacob, target.detach(), y.detach(), ints.detach()
+    assert not train, "no train label"
+    cifar100 = 'cifar100'
+    if dataset == cifar100 and valid:
+        return cifar100, 'x-valid'
+    if dataset == cifar100 and test:
+        return cifar100, 'x-test'
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    ImageNet16_120 = 'ImageNet16-120'
+    if dataset == ImageNet16_120 and valid:
+        return ImageNet16_120, 'x-valid'
+    if dataset == ImageNet16_120 and test:
+        return ImageNet16_120, 'x-test'
+    assert False, "Unknown dataset {args.dataset}"
+
+print(f"Initialize the train loader...")
+print(f"dataset = {args.dataset}, data location = {args.data_loc}, validation = {args.valid}")
+train_loader = datasets.get_data(args.dataset, args.data_loc, args.valid, args.batch_size, args.repeat, args)
+
+print(f"Initialize the nas bench api...")
+args.dataset, acc_type = remap_dataset_names(args.dataset, args.valid, args.test, args.train)
+print(f"dataset = {args.dataset}, validation = {args.valid}")
 searchspace = nasspace.get_search_space(args)
-train_loader = datasets.get_data(args.dataset, args.data_loc, args.trainval, args.batch_size, args.augtype, args.repeat, args)
+
+print(f"Making sure {args.save_loc} exist.")
 os.makedirs(args.save_loc, exist_ok=True)
 
+print(f"Currently calculate means and standards.")
+
+means, stds = get_mean_std(searchspace, args.n_samples, train_loader, device, args)
+
+
+print(f"Calculation of means and stds is done.")
+print(f"means = {means}\nstds = {stds}")
+print(f"========================================")
 
 times     = []
-chosen    = []
-acc       = []
-val_acc   = []
-topscores = []
-order_fn = np.nanargmax
-
-
-if args.dataset == 'cifar10':
-    acc_type = 'ori-test'
-    val_acc_type = 'x-valid'
-else:
-    acc_type = 'x-test'
-    val_acc_type = 'x-valid'
-
-
+accs = {"ni": [], "naswot": [], "logsynflow": [], "ninaswot": [], "rank-based": []}
+best_arch = {"ni": (0,0), "naswot": (0,0), "logsynflow": (0,0)}
 
 runs = trange(args.n_runs, desc='acc: ')
 for N in runs:
     start = time.time()
     indices = np.random.randint(0,len(searchspace),args.n_samples)
-    scores = []
 
+    scores = {"ni": [], "naswot": [], "logsynflow": [], "ninaswot": []}
     npstate = np.random.get_state()
     ranstate = random.getstate()
     torchstate = torch.random.get_rng_state()
     for arch in indices:
-        try:
-            uid = searchspace[arch]
-            network = searchspace.get_network(uid)
-            network.to(device)
-            if args.dropout:
-                add_dropout(network, args.sigma)
-            if args.init != '':
-                init_network(network, args.init)
-            if 'hook_' in args.score:
-                network.K = np.zeros((args.batch_size, args.batch_size))
-                def counting_forward_hook(module, inp, out):
-                    try:
-                        if not module.visited_backwards:
-                            return
-                        if isinstance(inp, tuple):
-                            inp = inp[0]
-                        inp = inp.view(inp.size(0), -1)
-                        x = (inp > 0).float()
-                        K = x @ x.t()
-                        K2 = (1.-x) @ (1.-x.t())
-                        network.K = network.K + K.cpu().numpy() + K2.cpu().numpy()
-                    except:
-                        pass
+        network = searchspace.get_network(arch)
+        network = network.to(device)
+        scores["ni"].append(ni_score(network, train_loader, device, args))
+        scores["naswot"].append(naswot_score(network, train_loader, device, args))
+        scores["logsynflow"].append(logsynflow_score(network, train_loader, device))
+        scores["ninaswot"].append(ninaswot_score(network, train_loader, device, stds, means, args))
+        del network
 
-
-                def counting_backward_hook(module, inp, out):
-                    module.visited_backwards = True
-
-
-                for name, module in network.named_modules():
-                    if 'ReLU' in str(type(module)):
-                        #hooks[name] = module.register_forward_hook(counting_hook)
-                        module.register_forward_hook(counting_forward_hook)
-                        module.register_backward_hook(counting_backward_hook)
-
-            random.setstate(ranstate)
-            np.random.set_state(npstate)
-            torch.set_rng_state(torchstate)
-
-            data_iterator = iter(train_loader)
-            x, target = next(data_iterator)
-            x2 = torch.clone(x)
-            x2 = x2.to(device)
-            x, target = x.to(device), target.to(device)
-            jacobs, labels, y, out = get_batch_jacobian(network, x, target, device, args)
-
-            if args.kernel:
-                s = get_score_func(args.score)(out, labels)
-            elif 'hook_' in args.score:
-                network(x2.to(device))
-                s = get_score_func(args.score)(network.K, target)
-            elif args.repeat < args.batch_size:
-                s = get_score_func(args.score)(jacobs, labels, args.repeat)
-            else:
-                s = get_score_func(args.score)(jacobs, labels)
-                
-        except Exception as e:
-            print(e)
-            s = 0.
-        
-        scores.append(s)
-
-    #print(len(scores))
-    #print(scores)
-    #print(order_fn(scores))
-
+    uid = indices[np.nanargmax(scores["ni"])]
+    accs["ni"].append(searchspace.get_final_accuracy(uid, acc_type, args.valid))
     
+    totrk = {}
+    rk_ni = indices[np.argsort(scores["ni"])]
+    for rk, id in enumerate(rk_ni):
+        if not id in totrk:
+            totrk[id] = len(rk_ni) - rk
+        else:
+            totrk[id] += len(rk_ni) - rk
+    
+    uid = indices[np.nanargmax(scores["naswot"])]
+    accs["naswot"].append(searchspace.get_final_accuracy(uid, acc_type, args.valid))
+    
+    rk_naswot = indices[np.argsort(scores["naswot"])]
+    for rk, id in enumerate(rk_naswot):
+        if not id in totrk:
+            totrk[id] = len(rk_naswot) - rk
+        else:
+            totrk[id] += len(rk_naswot) - rk
 
-    best_arch = indices[order_fn(scores)]
-    uid = searchspace[best_arch]
-    topscores.append(scores[order_fn(scores)])
-    chosen.append(best_arch)
-    #acc.append(searchspace.get_accuracy(uid, acc_type, args.trainval))
-    acc.append(searchspace.get_final_accuracy(uid, acc_type, False))
+    uid = indices[np.nanargmax(scores["logsynflow"])]
+    accs["logsynflow"].append(searchspace.get_final_accuracy(uid, acc_type, args.valid))
+    
+    rk_logsynflow = indices[np.argsort(scores["logsynflow"])]
+    bestrk = 1000000000000
+    for rk, id in enumerate(rk_logsynflow):
+        if not id in totrk:
+            totrk[id] = len(rk_logsynflow) - rk
+        else:
+            totrk[id] += len(rk_logsynflow) - rk
+        if bestrk > totrk[id]:
+            bestrk = totrk[id]
+            bestrk_uid = id
 
-    if not args.dataset == 'cifar10' or args.trainval:
-        val_acc.append(searchspace.get_final_accuracy(uid, val_acc_type, args.trainval))
-    #    val_acc.append(info.get_metrics(dset, val_acc_type)['accuracy'])
+    accs["rank-based"].append(searchspace.get_final_accuracy(bestrk_uid, acc_type, args.valid))
+
+    uid = indices[np.nanargmax(scores["ninaswot"])]
+    accs["ninaswot"].append(searchspace.get_final_accuracy(uid, acc_type, args.valid))
 
     times.append(time.time()-start)
-    runs.set_description(f"acc: {mean(acc):.2f}% time:{mean(times):.2f}")
+    runs.set_description(f"acc_ni: {mean(accs['ni']):.2f}({std(accs['ni']):.3f}), acc_naswot: {mean(accs['naswot']):.2f}({std(accs['naswot']):.3f}), acc_logsyn: {mean(accs['logsynflow']):.2f}({std(accs['logsynflow']):.3f}), acc_ninaswot: {mean(accs['ninaswot']):.2f}({std(accs['ninaswot']):.3f}), rk-based: {mean(accs['rank-based']):.2f}({std(accs['rank-based']):.3f})")
 
-print(f"Final mean test accuracy: {np.mean(acc)}")
-#if len(val_acc) > 1:
-#    print(f"Final mean validation accuracy: {np.mean(val_acc)}")
-
+"""
 state = {'accs': acc,
          'chosen': chosen,
          'times': times,
@@ -188,3 +180,4 @@ state = {'accs': acc,
 dset = args.dataset if not (args.trainval and args.dataset == 'cifar10') else 'cifar10-valid'
 fname = f"{args.save_loc}/{args.save_string}_{args.score}_{args.nasspace}_{dset}_{args.kernel}_{args.dropout}_{args.augtype}_{args.sigma}_{args.repeat}_{args.batch_size}_{args.n_runs}_{args.n_samples}_{args.seed}.t7"
 torch.save(state, fname)
+"""
--- search/GA_based_on_rank.py
+++ search/GA_based_on_rank.py
@@ -6,7 +6,7 @@ import numpy as np
 
 class chromosome():
     def __init__(self, gene = None, fitness = None, acc = None, uid = None):
-        """ fitness: (ninaswot, ntk,..., tot) """
+        """ fitness: (naswot, ntk, ni, logsynflow, tot) """
         self.gene = gene
         self.fitness = fitness
         self.acc = acc
@@ -14,22 +14,6 @@ class chromosome():
 
 class GA():
     def __init__(self, MAXN_CONNECTION, MAXN_OPERATION, searchspace, train_loader, device, stds, means, acc_type, args):
-        #### cheat
-        if args.valid:
-            base_loc  = f"~/Genetic-Based-Neural-Architecture-Search-with-Hybrid-Score-Functions/results/score/{args.dataset}"
-        elif args.test:
-            base_loc  = f"~/Genetic-Based-Neural-Architecture-Search-with-Hybrid-Score-Functions/results/score/{args.dataset}-test"
-
-        self.ninaswot = np.load(os.path.expanduser(f"{base_loc}/ninaswot_nasbench201_{args.dataset}_none_0.05_1_{args.valid}_128_1_1.npy"))
-        self.ntk      = np.load(os.path.expanduser(f"{base_loc}/ntk_nasbench201_{args.dataset}_none_0.05_1_{args.valid}_128_1_1.npy"))
-        #self.synflow  = np.load(f"{base_loc}/synflow_nasbench201_{args.dataset}_none_0.05_1_{args.valid}_128_1_1.npy")
-        self.logsynflow  = np.load(os.path.expanduser(f"{base_loc}/logsynflow_nasbench201_{args.dataset}_none_0.05_1_{args.valid}_128_1_1.npy"))
-        ####
-
-        assert len(self.ninaswot) == 15625, "broken: ninaswot"
-        assert len(self.ntk) == 15625, "broken: ntk"
-        #assert len(self.synflow) == 15625, "broken: synflow"
-        ####
         self.MAXN_POPULATION = args.maxn_pop
         self.MAXN_ITERATION = args.maxn_iter
         self.PROB_MUTATION = args.prob_mut
@@ -45,50 +29,56 @@ class GA():
         self.stds = stds
         self.means = means
         self.acc_type = acc_type
-        self.best_chrom = chromosome()
-        self.candiate = {"ninaswot":set(), "ntk":set(), "logsynflow":set(), "tot":[]}
+        self.best_chrom = {"ni": chromosome(), "naswot": chromosome(), "logsynflow": chromosome(), "ninaswot": chromosome()}
+        self.candiate = {"ninaswot":set(), "ntk":set(), "logsynflow":set(), "naswot":set(), "ni":set(), "tot":[]}
         self.NAS_201_ops = ['none', 'skip_connect', 'nor_conv_1x1', 'nor_conv_3x3', 'avg_pool_3x3']
 
     def init_population(self):
         self.population = [chromosome("".join([(bin(1<<random.randint(0, self.MAXN_OPERATION-1))[2:]).zfill(self.MAXN_OPERATION) for j in range(self.MAXN_CONNECTION)])) for i in range(self.MAXN_POPULATION)]
-        """
-        for i in range(self.MAXN_POPULATION):
-            chrom = chromosome("".join([(bin(1<<random.randint(0, self.MAXN_OPERATION-1))[2:]).zfill(self.MAXN_OPERATION) for j in range(self.MAXN_CONNECTION)]))
-            #for j in range(self.MAXN_CONNECTION):
-            #    op = random.randint(0, self.MAXN_OPERATION-1)
-            #    chrom.gene += (bin(1<<op)[2:]).zfill(self.MAXN_OPERATION)
-            self.population.append(chrom)
-        """
 
     def evaluate(self):
         for i in range(len(self.population)):
             network, uid, acc = gene2net(self.population[i].gene, self.NAS_201_ops, self.searchspace, self.acc_type, self.args.valid)
-            del network
+            #del network
             self.population[i].acc = acc
             self.population[i].uid = uid
             if self.population[i].uid not in self.DICT:
                 uid = self.population[i].uid
-                #self.population[i].fitness = self.DICT[self.population[i].gene] = \
-                #(standardize(ninaswot_score(network, self.train_loader, self.device, self.stds, self.means, self.args), self.means["ninaswot"], self.stds["ninaswot"]), \
+                """ fitness: (naswot, ni, ntk, logsynflow, ninaswot) """
+                x = [naswot_score(network, self.train_loader, self.device, self.args), \
+                ni_score(network, self.train_loader, self.device, self.args), \
+                0, \
+                logsynflow_score(network, self.train_loader, self.device)]
+                x.append(standardize(x[0], self.means["naswot"], self.stds["naswot"])*2+standardize(x[1], self.means["ni"], self.stds["ni"]))
                 #-standardize(ntk_score(network, self.train_loader, self.device), self.means["ntk"], self.stds["ntk"]), \
-                #standardize(entropy_score(network, self.train_loader, self.device, self.args), self.means["entropy"], self.stds["entropy"]))
-                x = [self.ninaswot[uid], self.ntk[uid], 0.1*self.logsynflow[uid], 0]
-                for j in range(len(x)):
-                    x[j] = x[j] if np.isfinite(x[j]) else -np.inf
-                x[-1] = sum(x)
-                
+
                 self.population[i].fitness = self.DICT[uid] = tuple(x)
             else:
                 self.population[i].fitness = self.DICT[self.population[i].uid]
-
-            """
+            del network
+            
             # global optimum
-            if (sum(self.population[i].fitness) > sum(self.best_chrom.fitness) or self.best_chrom.gene == "") and self.population[i].fitness >= (0,0,0) and self.population[i].fitness <= (50,50,50):
-                self.best_chrom.fitness = self.population[i].fitness
-                self.best_chrom.acc = self.population[i].acc
-                self.best_chrom.uid = self.population[i].uid
-                self.best_chrom.gene = copy.deepcopy(self.population[i].gene)
-            """
+            if self.args.verbose:
+                if self.best_chrom["naswot"].gene == None or self.population[i].fitness[0] > self.best_chrom["naswot"].fitness[0]:
+                    self.best_chrom["naswot"].fitness = self.population[i].fitness
+                    self.best_chrom["naswot"].acc = self.population[i].acc
+                    self.best_chrom["naswot"].uid = self.population[i].uid
+                    self.best_chrom["naswot"].gene = copy.deepcopy(self.population[i].gene)
+                if self.best_chrom["ni"].gene == None or self.population[i].fitness[1] > self.best_chrom["ni"].fitness[1]:
+                    self.best_chrom["ni"].fitness = self.population[i].fitness
+                    self.best_chrom["ni"].acc = self.population[i].acc
+                    self.best_chrom["ni"].uid = self.population[i].uid
+                    self.best_chrom["ni"].gene = copy.deepcopy(self.population[i].gene)
+                if self.best_chrom["logsynflow"].gene == None or self.population[i].fitness[3] > self.best_chrom["logsynflow"].fitness[3]:
+                    self.best_chrom["logsynflow"].fitness = self.population[i].fitness
+                    self.best_chrom["logsynflow"].acc = self.population[i].acc
+                    self.best_chrom["logsynflow"].uid = self.population[i].uid
+                    self.best_chrom["logsynflow"].gene = copy.deepcopy(self.population[i].gene)
+                if self.best_chrom["ninaswot"].gene == None or self.population[i].fitness[4] > self.best_chrom["ninaswot"].fitness[4]:
+                    self.best_chrom["ninaswot"].fitness = self.population[i].fitness
+                    self.best_chrom["ninaswot"].acc = self.population[i].acc
+                    self.best_chrom["ninaswot"].uid = self.population[i].uid
+                    self.best_chrom["ninaswot"].gene = copy.deepcopy(self.population[i].gene)
 
     def mutation(self, chrom):
         if chrom == None: return None
@@ -133,6 +123,12 @@ class GA():
         """
         return chromosome("".join(newgenelist0)), chromosome("".join(newgenelist1))
 
+    def add_candiates_by(self, idx, metrice):
+        self.population.sort(key = lambda this: this.fitness[idx], reverse = True)
+        for met in metrice:
+            self.candiate[met].add(self.population[0].uid)
+            self.candiate[met].add(self.population[1].uid)
+
     def find_best(self):
         self.init_population()
         self.evaluate()
@@ -161,51 +157,53 @@ class GA():
             self.evaluate()
             offsprings = self.population
             self.population = elder + offsprings
-
-            self.population.sort(key = lambda this: this.fitness[0], reverse = True) # ninaswot rank
-            self.candiate["ninaswot"].add(self.population[0].uid)
-            self.candiate["ninaswot"].add(self.population[1].uid)
-            self.candiate["ninaswot"].add(self.population[2].uid)
-
-            self.population.sort(key = lambda this: this.fitness[1], reverse = True) # ntk rank
-            self.candiate["ntk"].add(self.population[0].uid)
-            self.candiate["ntk"].add(self.population[1].uid)
-            self.candiate["ntk"].add(self.population[2].uid)
             
-            self.population.sort(key = lambda this: this.fitness[2], reverse = True) # logsynflow rank
-            self.candiate["logsynflow"].add(self.population[0].uid)
-            self.candiate["logsynflow"].add(self.population[1].uid)
-            self.candiate["logsynflow"].add(self.population[2].uid)
-            
+            self.add_candiates_by(0, ["naswot", "ni", "ntk", "logsynflow"])
+            self.add_candiates_by(1, ["naswot", "ni", "ntk", "logsynflow"])
+            #self.add_candiates_by(2, ["naswot", "ni", "ntk", "logsynflow"])
+            self.add_candiates_by(3, ["naswot", "ni", "ntk", "logsynflow"])
+
             self.population = elder + offsprings
         
-        self.candiate["tot"].extend(list(self.candiate["ninaswot"]))
-        self.candiate["tot"].extend(list(self.candiate["ntk"]))
-        self.candiate["tot"].extend(list(self.candiate["logsynflow"]))
-        self.candiate["tot"].sort(key = lambda uid: self.DICT[uid][-1], reverse = True)
-        best_uid = self.candiate["tot"][0]
-        network, uid, acc = uid2net(best_uid, self.searchspace, self.acc_type, self.args.valid)
-        #offsprings.sort(key = lambda this: this.fitness[0], reverse = True)
-        """ rank-based
-        ninaswot_rk = sorted(list(self.candiate["ninaswot"]), key = lambda this: self.DICT[this][0], reverse = True)
-        ntk_rk      = sorted(list(self.candiate["ntk"]), key = lambda this: self.DICT[this][1], reverse = True)
+        naswot_rk     = sorted(list(self.candiate["naswot"]), key = lambda this: self.DICT[this][0], reverse = True)
+        ni_rk         = sorted(list(self.candiate["ni"]), key = lambda this: self.DICT[this][1], reverse = True)
+        #ntk_rk        = sorted(list(self.candiate["ntk"]), key = lambda this: self.DICT[this][2], reverse = True)
+        logsynflow_rk = sorted(list(self.candiate["logsynflow"]), key = lambda this: self.DICT[this][3], reverse = True)
         rank = {}
         best_rank = None
-        for rk, uid in enumerate(ninaswot_rk):
+        for rk, uid in enumerate(naswot_rk):
             rank[uid] = rk+1
+        for rk, uid in enumerate(ni_rk):
+            if uid in rank:
+                rank[uid] += rk+1
+            else:
+                rank[uid] = rk+1
+        """
         for rk, uid in enumerate(ntk_rk):
-            rank[uid] += rk+1
+            if uid in rank:
+                rank[uid] += rk+1
+            else:
+                rank[uid] = rk+1
+        """
+        maxacc = -1
+        for rk, uid in enumerate(logsynflow_rk):
+            if uid in rank:
+                rank[uid] += rk+1
+            else:
+                rank[uid] = rk+1
+            maxacc = max(maxacc, self.searchspace.get_final_accuracy(uid, self.acc_type, self.args.valid))
             if best_rank == None or best_rank > rank[uid]:
                 best_rank = rank[uid]
                 best_uid = uid
         
-        #print(f"select best rank: {best_rank}")
         network, uid, acc = uid2net(best_uid, self.searchspace, self.acc_type, self.args.valid)
-        """
-
+        
         #else:
         #    network, uid, acc = gene2net(offsprings[0].gene, self.NAS_201_ops, self.searchspace, self.acc_type, self.args.valid)
-        return self.DICT[uid], acc, uid
+        if self.args.verbose:
+            return self.DICT[uid], acc, uid, best_rank, self.best_chrom["naswot"].acc, self.best_chrom["ni"].acc, self.best_chrom["logsynflow"].acc, self.best_chrom["ninaswot"].acc
+        else:
+            return self.DICT[uid], acc, uid, best_rank
 
 def gene2sect(gene, ops):
     gene_sect_len = len(ops)
