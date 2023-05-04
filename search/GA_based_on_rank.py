e45f151d3520da113749f3b1cd5037a550d22693
OEmiliatanO <emiliaistruelove@gmail.com>
2023-05-05 06:36:46 +0800

    get on natsbenchsss

--- search/GA_based_on_rank.py
+++ search/GA_based_on_rank.py
@@ -32,22 +32,28 @@ class GA():
         self.best_chrom = {"ni": chromosome(), "naswot": chromosome(), "logsynflow": chromosome(), "ninaswot": chromosome()}
         self.candiate = {"ninaswot":set(), "ntk":set(), "logsynflow":set(), "naswot":set(), "ni":set(), "tot":[]}
         self.NAS_201_ops = ['none', 'skip_connect', 'nor_conv_1x1', 'nor_conv_3x3', 'avg_pool_3x3']
+        self.NATS_SSS_ops = [8, 16, 24, 32, 40, 48, 56, 64]
 
     def init_population(self):
-        self.population = [chromosome("".join([(bin(1<<random.randint(0, self.MAXN_OPERATION-1))[2:]).zfill(self.MAXN_OPERATION) for j in range(self.MAXN_CONNECTION)])) for i in range(self.MAXN_POPULATION)]
+        if self.args.nasspace == 'natsbenchSSS':
+            self.population = [chromosome(random.sample(range(0,len(self.NATS_SSS_ops)), k=5)) for i in range(self.MAXN_POPULATION)]
+        elif self.args.nasspace == 'nasbench201':
+            self.population = [chromosome("".join([(bin(1<<random.randint(0, self.MAXN_OPERATION-1))[2:]).zfill(self.MAXN_OPERATION) for j in range(self.MAXN_CONNECTION)])) for i in range(self.MAXN_POPULATION)]
 
     def evaluate(self):
         for i in range(len(self.population)):
-            network, uid, acc = gene2net(self.population[i].gene, self.NAS_201_ops, self.searchspace, self.acc_type, self.args.valid)
+            if self.args.nasspace == 'natsbenchSSS':
+                network, uid, acc = gene2net(self.population[i].gene, self.NATS_SSS_ops, self.searchspace, self.acc_type, self.args.valid, self.args)
+            elif self.args.nasspace == 'nasbench201':
+                network, uid, acc = gene2net(self.population[i].gene, self.NAS_201_ops, self.searchspace, self.acc_type, self.args.valid, self.args)
             #del network
             self.population[i].acc = acc
             self.population[i].uid = uid
             if self.population[i].uid not in self.DICT:
                 uid = self.population[i].uid
-                """ fitness: (naswot, ni, ntk, logsynflow) """
+                """ fitness: (naswot, ni, logsynflow) """
                 x = [naswot_score(network, self.train_loader, self.device, self.args), \
                 ni_score(network, self.train_loader, self.device, self.args), \
-                0, \
                 logsynflow_score(network, self.train_loader, self.device)]
 
                 self.population[i].fitness = self.DICT[uid] = tuple(x)
@@ -75,46 +81,68 @@ class GA():
 
     def mutation(self, chrom):
         if chrom == None: return None
-        p = random.randint(0, self.MAXN_CONNECTION-1)
-        gene_sect_len = self.MAXN_OPERATION
-        gene = chrom.gene
-        genelist = [gene[i:i+gene_sect_len] for i in range(0, len(gene), gene_sect_len)]
-        idx = genelist[p].find('1')
-        newop = random.choice([i for i in range(idx)] + [i for i in range(idx + 1, self.MAXN_OPERATION)])
-        genelist[p] = (bin(1<<newop)[2:]).zfill(self.MAXN_OPERATION)
-        chrom.gene = "".join(genelist)
+        if self.args == "natsbenchSSS":
+            p = random.randint(0, 7)
+            chrom.gene[p] = random.randint(0, 7)
+        elif self.args == "nasbench201":
+            p = random.randint(0, self.MAXN_CONNECTION-1)
+            gene_sect_len = self.MAXN_OPERATION
+            gene = chrom.gene
+            genelist = [gene[i:i+gene_sect_len] for i in range(0, len(gene), gene_sect_len)]
+            idx = genelist[p].find('1')
+            newop = random.choice([i for i in range(idx)] + [i for i in range(idx + 1, self.MAXN_OPERATION)])
+            genelist[p] = (bin(1<<newop)[2:]).zfill(self.MAXN_OPERATION)
+            chrom.gene = "".join(genelist)
         return chrom
     
     def select_2chrom_fromN(self, N=3):
         cand = random.sample(self.population, N)
-        maxi = smaxi = cand[0]
-        for chrom in cand:
-            if maxi.fitness[-1] < chrom.fitness[-1]:
-                smaxi = maxi
-                maxi = chrom
-            elif smaxi.fitness[-1] < chrom.fitness[-1]:
-                smaxi = chrom
-        return maxi, smaxi
+        totrk = dict(zip([chrom.uid for chrom in cand], [0 for i in range(N)]))
+        rk_naswot     = sorted(cand, key = lambda x: x.fitness[0], reverse = True)
+        rk_ni         = sorted(cand, key = lambda x: x.fitness[1], reverse = True)
+        rk_logsynflow = sorted(cand, key = lambda x: x.fitness[2], reverse = True)
+        for rk, chrom in enumerate(rk_ni):
+            totrk[chrom.uid] += rk
+        
+        for rk, chrom in enumerate(rk_naswot):
+            totrk[chrom.uid] += rk
+
+        bestrk = np.inf
+        for rk, chrom in enumerate(rk_logsynflow):
+            totrk[chrom.uid] += rk
+
+        chosen = [*map(lambda x: x[0], sorted(totrk.items(), key = lambda x: x[1])[:2])]
+        res = [*filter(lambda x: x.uid == chosen[0] or x.uid == chosen[1], cand)]
+        return res
 
     def crossover(self, p0, p1):
-        gene_sect_len = self.MAXN_OPERATION
-        genelist0 = [p0.gene[i:i+gene_sect_len] for i in range(0, len(p0.gene), gene_sect_len)]
-        genelist1 = [p1.gene[i:i+gene_sect_len] for i in range(0, len(p1.gene), gene_sect_len)]
-        
-        l0 = random.randint(1, self.MAXN_CONNECTION)
-        r0 = random.choice([i for i in range(l0)] + [i for i in range(l0 + 1, self.MAXN_CONNECTION)])
-        if l0>r0: l0,r0 = r0, l0
-        l1 = random.randint(1, self.MAXN_CONNECTION)
-        r1 = random.choice([i for i in range(l1)] + [i for i in range(l1 + 1, self.MAXN_CONNECTION)])
-        if l1>r1: l1,r1 = r1, l1
-        newgenelist0 = genelist0[:l1] + genelist1[l1:r1] + genelist0[r1:]
-        newgenelist1 = genelist1[:l0] + genelist0[l0:r0] + genelist1[r0:]
-        """
-        break_point = random.randint(1, self.MAXN_CONNECTION-1)
-        newgenelist0 = genelist0[:break_point] + genelist1[break_point:]
-        newgenelist1 = genelist1[:break_point] + genelist0[break_point:]
-        """
-        return chromosome("".join(newgenelist0)), chromosome("".join(newgenelist1))
+        if self.args.nasspace == 'natsbenchSSS':
+            l0 = random.randint(1, 8)
+            r0 = random.choice([i for i in range(l0)] + [i for i in range(l0 + 1, 8)])
+            if l0>r0: l0,r0 = r0, l0
+            l1 = random.randint(1, 8)
+            r1 = random.choice([i for i in range(l1)] + [i for i in range(l1 + 1, 8)])
+            if l1>r1: l1,r1 = r1, l1
+            newgene0 = p0.gene[:l1] + p1.gene[l1:r1] + p0.gene[r1:]
+            newgene1 = p1.gene[:l0] + p0.gene[l0:r0] + p1.gene[r0:]
+            return chromosome(newgene0), chromosome(newgene1)
+        elif self.args.nasspace == 'nasbench201':
+            gene_sect_len = self.MAXN_OPERATION
+            genelist0 = [p0.gene[i:i+gene_sect_len] for i in range(0, len(p0.gene), gene_sect_len)]
+            genelist1 = [p1.gene[i:i+gene_sect_len] for i in range(0, len(p1.gene), gene_sect_len)]
+            
+            l0 = random.randint(1, self.MAXN_CONNECTION)
+            r0 = random.choice([i for i in range(l0)] + [i for i in range(l0 + 1, self.MAXN_CONNECTION)])
+            if l0>r0: l0,r0 = r0, l0
+            l1 = random.randint(1, self.MAXN_CONNECTION)
+            r1 = random.choice([i for i in range(l1)] + [i for i in range(l1 + 1, self.MAXN_CONNECTION)])
+            if l1>r1: l1,r1 = r1, l1
+            newgenelist0 = genelist0[:l1] + genelist1[l1:r1] + genelist0[r1:]
+            newgenelist1 = genelist1[:l0] + genelist0[l0:r0] + genelist1[r0:]
+
+            return chromosome("".join(newgenelist0)), chromosome("".join(newgenelist1))
+        assert False, f"doesn't support such nasspace: {self.args.nasspace}"
+        return None
 
     def add_candiates_by(self, idx, metrice):
         self.population.sort(key = lambda this: this.fitness[idx], reverse = True)
@@ -151,17 +179,15 @@ class GA():
             offsprings = self.population
             self.population = elder + offsprings
             
-            self.add_candiates_by(0, ["naswot", "ni", "ntk", "logsynflow"])
-            self.add_candiates_by(1, ["naswot", "ni", "ntk", "logsynflow"])
-            #self.add_candiates_by(2, ["naswot", "ni", "ntk", "logsynflow"])
-            self.add_candiates_by(3, ["naswot", "ni", "ntk", "logsynflow"])
+            self.add_candiates_by(0, ["naswot", "ni", "logsynflow"])
+            self.add_candiates_by(1, ["naswot", "ni", "logsynflow"])
+            self.add_candiates_by(2, ["naswot", "ni", "logsynflow"])
 
             self.population = elder + offsprings
         
         rk_naswot     = sorted(list(self.candiate["naswot"]), key = lambda this: self.DICT[this][0], reverse = True)
         rk_ni         = sorted(list(self.candiate["ni"]), key = lambda this: self.DICT[this][1], reverse = True)
-        #rk_ntk        = sorted(list(self.candiate["ntk"]), key = lambda this: self.DICT[this][2], reverse = True)
-        rk_logsynflow = sorted(list(self.candiate["logsynflow"]), key = lambda this: self.DICT[this][3], reverse = True)
+        rk_logsynflow = sorted(list(self.candiate["logsynflow"]), key = lambda this: self.DICT[this][2], reverse = True)
 
         totrk = dict(zip([uid for uid in self.candiate["naswot"]], [0 for i in range(len(rk_naswot))]))
     
@@ -179,7 +205,7 @@ class GA():
                 bestrk = totrk[id]
                 bestrk_uid = id
                
-        network, uid, acc = uid2net(bestrk_uid, self.searchspace, self.acc_type, self.args.valid)
+        network, uid, acc = uid2net(bestrk_uid, self.searchspace, self.acc_type, self.args.valid, self.args)
         
         if self.args.verbose:
             return self.DICT[uid], acc, uid, best_rank, self.best_chrom["naswot"].acc, self.best_chrom["ni"].acc, self.best_chrom["logsynflow"].acc, self.best_chrom["ninaswot"].acc
@@ -190,16 +216,18 @@ def gene2sect(gene, ops):
     gene_sect_len = len(ops)
     return [ops[gene[i:i+gene_sect_len].find("1")] for i in range(0, len(gene), gene_sect_len)]
 
-def gene2net(gene, ops, searchspace, acc_type, valid):
-    gene_sect = gene2sect(gene, ops)
-    arch = "|{}~0|+|{}~0|{}~1|+|{}~0|{}~1|{}~2|".format(*gene_sect)
-    idx = searchspace.query_index_by_arch(arch)
-    uid = searchspace[idx]
-    network = searchspace.get_network(uid)
+def gene2net(gene, ops, searchspace, acc_type, valid, args):
+    if args.nasspace == "natsbenchSSS":
+        arch = "{}:{}:{}:{}:{}".format(*map(lambda x: ops[x], gene))
+    elif args.nasspace == "nasbench201":
+        gene_sect = gene2sect(gene, ops)
+        arch = "|{}~0|+|{}~0|{}~1|+|{}~0|{}~1|{}~2|".format(*gene_sect)
+    uid = searchspace.query_index_by_arch(arch)
+    network = searchspace.get_network(uid, args)
     acc = searchspace.get_final_accuracy(uid, acc_type, valid)
     return network, uid, acc
 
-def uid2net(uid, searchspace, acc_type, valid):
-    network = searchspace.get_network(uid)
+def uid2net(uid, searchspace, acc_type, valid, args):
+    network = searchspace.get_network(uid, args)
     acc = searchspace.get_final_accuracy(uid, acc_type, valid)
     return network, uid, acc
