c7ed048da3c9b75e24632af24783bc97180d1d19
OEmiliatanO <emiliaistruelove@gmail.com>
2023-06-10 18:25:44 +0800

    use encoding

--- search/SA/nasbench201_SA_rk.py
+++ search/SA/nasbench201_SA_rk.py
@@ -6,21 +6,25 @@ from tqdm import trange
 import time
 from score import *
 from .SA_rk_abstract import abstract_SA
+from encoder import encoder
 
 class SA(abstract_SA):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.NAS_201_ops = ['none', 'skip_connect', 'nor_conv_1x1', 'nor_conv_3x3', 'avg_pool_3x3']
+        self.Encoder = encoder.get_encoder("nasbench201")
 
     def neighbor(self, arch):
-        new_arch = arch
+        l1, l2, l3 = arch[:6], arch[6], arch[7:]
         pos = random.sample([*range(6)], random.randint(0, 5))
         for p in pos:
-            new_arch[p] = (new_arch[p] + random.randint(0, 4)) % 5
-        return new_arch
+            l1[p] = (l1[p] + random.randint(0, 4)) % 5
+
+        return l1+l2+l3
 
     def rand_arch_generate(self):
-        return [random.choice([*range(5)]) for i in range(6)]
+        return self.Encoder.get_rand_code()
 
     def list2arch(self, l):
-        return "|{}~0|+|{}~0|{}~1|+|{}~0|{}~1|{}~2|".format(*map(lambda op: self.NAS_201_ops[op], l))
+        l = list(l)
+        return self.searchspace.get_arch_str_by_code(self.Encoder.parse_code(l))
