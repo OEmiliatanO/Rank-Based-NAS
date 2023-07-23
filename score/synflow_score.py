1c39b8e88fcd21a5e37766810137854065574035
OEmiliatanO <emiliaistruelove@gmail.com>
2023-07-23 21:48:54 +0800

    detach unused tensor, to avoid mem leak

--- score/synflow_score.py
+++ score/synflow_score.py
@@ -1,3 +1,18 @@
+# Copyright 2021 Samsung Electronics Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
+
 import torch
 import torch.nn as nn
 import numpy as np
@@ -47,9 +62,10 @@ def synflow_score(network, train_loader, device, args):
 
     score = 0
     for i in range(len(grads)):
+        grads[i].detach()
         score += torch.sum(grads[i])
 
-    del data
-    del network
+    score = score.detach().cpu().numpy()
+    del network, data, _, grads, output
     
-    return score.detach().cpu().numpy()
+    return score
