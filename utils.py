3213ccbec199add2a22b0a540df516df502639b5
OEmiliatanO <emiliaistruelove@gmail.com>
2023-07-23 21:49:32 +0800

    add functions for snip, grasp, fisher

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import torch
 from pycls.models.nas.nas import Cell
+import torch.nn as nn
 
 class DropChannel(torch.nn.Module):
     def __init__(self, p, mod):
@@ -121,3 +122,29 @@ def remap_dataset_names(dataset, valid, test, train):
     if dataset == ImageNet16_120 and test:
         return ImageNet16_120, 'x-test'
     assert False, "Unknown dataset {args.dataset}"
+
+
+def get_layer_metric_array(net, metric, mode): 
+    metric_array = []
+
+    for layer in net.modules():
+        if mode=='channel' and hasattr(layer,'dont_ch_prune'):
+            continue
+        if isinstance(layer, nn.Conv2d) or isinstance(layer, nn.Linear):
+            metric_array.append(metric(layer))
+    
+    return metric_array
+
+def reshape_elements(elements, shapes, device):
+    def broadcast_val(elements, shapes):
+        ret_grads = []
+        for e,sh in zip(elements, shapes):
+            ret_grads.append(torch.stack([torch.Tensor(sh).fill_(v) for v in e], dim=0).to(device))
+        return ret_grads
+    if type(elements[0]) == list:
+        outer = []
+        for e,sh in zip(elements, shapes):
+            outer.append(broadcast_val(e,sh))
+        return outer
+    else:
+        return broadcast_val(elements, shapes)
