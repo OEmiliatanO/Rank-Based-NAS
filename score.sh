a702886428bdc973f1442bee45b122299c7b5d6e
OEmiliatanO <emiliaistruelove@gmail.com>
2024-02-28 09:57:23 +0800

    update score script

---
+++ score.sh
@@ -0,0 +1,3 @@
+python score.py --save_string score --augtype none --repeat 1 --sigma 1 --nasspace nasbench201 --api_loc ../NATS-tss-v1_0-3ffb9-simple/ --batch_size 128 --GPU 0 --n_runs 1 --data_loc ./cifardata/ --dataset cifar10 --test
+
+#python score.py --save_string score --augtype none --repeat 1 --sigma 1 --nasspace natsbenchsss --api_loc ../NATS-sss-v1_0-50262-simple/ --batch_size 128 --GPU 0 --n_runs 1 --data_loc ./cifardata/ --dataset cifar10 --test
